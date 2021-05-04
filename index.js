f5504e51c87abb7dfa773c0ff30ffee88d9e50f0
saurabh <sourabhkt19@gmail.com>
2021-05-04 16:36:03 +0530

    function created: lcl, ucl | function changed: pChart

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-const data = 347;
-const Pchart = (data)=>{
-    
-    // lcl = p - m * sqrt(p(1-p)/n) 
+   // lcl = p - m * sqrt(p(1-p)/n) 
     // cl = p
     // ucl =  p + m * sqrt(p(1-p)/n) 
 //    const totalValue =  data.reduce((accumulator, item)=>{
 //         accumulator = accumulator+item;
 //         return accumulator;
 //     }, 0);
-    const m = 30, n =50; 
-    const len = 50;
-    const p = data/(m*n);
-    console.log(p);
+const ucl = (p, n)=>{
+    const UCL = p + (3 * Math.sqrt((p*Math.abs(1-p))/n)); 
+    return UCL;
+}
+
+const lcl = (p,n)=>{
+    const LCL = p - (3 * Math.sqrt((p*(1-p))/n)); 
+    return LCL;
+}
+const data = [12,15,8, 10, 4, 7, 16, 9, 14, 10, 5, 6, 17, 12, 22 ,8,10, 5 , 13, 11, 20, 18, 24, 15, 9, 12, 7, 13, 9, 6];
+
+const pChart = (data, sampleSize)=>{
+    
+    const totalItem = data.reduce((accumulator, item)=>{
+        return accumulator+item;
+    }, 0);
+    const details = {
+        lcl:0,
+        cl:0 ,
+        ucl:0,
+        sampleData: []
+    };
+
+ 
+    const m = data.length, n = sampleSize; 
+    const p = totalItem/(m*n);
     const cl = p;
-    const ucl = p + (3 * Math.sqrt((p*Math.abs(1-p))/len)); 
-    const lcl = p - (3 * Math.sqrt((p*(1-p))/len)); 
-    console.log(ucl); 
-    console.log(lcl);
+    const upperControlLimit = ucl(p,n);
+    const lowerControlLimit = lcl(p,n);
+    
+    details.cl = cl;
+    details.ucl = upperControlLimit;
+    details.lcl = lowerControlLimit;
+
+    data.map((item, sampleNumber)=>{
+        let pi = item / n;
+        details.sampleData.push({x: sampleNumber+1, y: pi});
+    });
+
+    return details;
     
 
 }
 
-Pchart(data);
+const details = pChart(data, 50);
+console.log(details);
